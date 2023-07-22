70691fb2fcbaa061d8b9bfb027320bd4029bdf92
TurtlePower <agnkvdm@gmail.com>
2023-07-22 14:23:52 +0300

    fix logged in home page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,11 @@
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { LoginWrapper, HeroWrapper, Decription, LinkItem } from './Hero.styled';
 import { Container } from 'components/Container/Container';
+import { selectIsLoggedIn } from 'redux/auth/selectors';
 
 export const Hero = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <Container>
       <HeroWrapper>
@@ -12,15 +15,23 @@ export const Hero = () => {
           with dreaming and thinking about new horizons instead!
         </Decription>
       </HeroWrapper>
-
       <LoginWrapper>
-        <p>
-          <LinkItem to="/login">Login</LinkItem> to your account to get access
-          to all your contacts
-        </p>
-        <p>
-          Isn't member yet? <LinkItem to="/register">Sign up</LinkItem>
-        </p>
+        {isLoggedIn ? (
+          <div>
+            Wanna call someone?{' '}
+            <LinkItem to="/contacts">Find number here</LinkItem>
+          </div>
+        ) : (
+          <>
+            <p>
+              <LinkItem to="/login">Login</LinkItem> to your account to get
+              access to all your contacts
+            </p>
+            <p>
+              Isn't member yet? <LinkItem to="/register">Sign up</LinkItem>
+            </p>
+          </>
+        )}
       </LoginWrapper>
     </Container>
   );
