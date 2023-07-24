4ac03ca54fe79d7ea16fafbc6d9296bc47cb7f2d
made-by-curiosity <93835953+made-by-curiosity@users.noreply.github.com>
2023-07-24 09:47:09 +0000

    Deploying to gh-pages from @ made-by-curiosity/phonebook@348de39ce736c4de17eb9ecff582d4dda117d28d 🚀

---
+++ static/js/856.6ba83c45.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook=self.webpackChunkphonebook||[]).push([[856],{6856:function(t,n,h){h.d(n,{ZkW:function(){return e}});var a=h(9983);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"}}]})(t)}}}]);
+//# sourceMappingURL=856.6ba83c45.chunk.js.map
--- static/js/856.e1d37cfe.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[856],{6856:function(t,h,n){n.d(h,{ZkW:function(){return e}});var a=n(9983);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"}}]})(t)}}}]);
-//# sourceMappingURL=856.e1d37cfe.chunk.js.map
